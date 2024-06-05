209f0b54508ab91d54788edd33dc41b14296d2ce
umeshmahato28 <mahatoumesh94@gmail.com>
2024-06-05 16:25:10 +0530

    add images

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
+
 var React = require("react");
 var ReactDom = require("react-dom");
 
-ReactDom.render(<h1>Hello World</h1>, document.getElementById("root"));
+
+
+const img1="https://picsum.photos/200/300";
+const links="https://www.amazon.in";
+
+const fname="rahu raj";
+const heading={
+      color:"orange",
+      textTransform:"capitalize",
+      textAlign:"center",
+      
+      
+}
+ReactDom.render(
+      <>
+<h1  style={heading}
+      contentEditable="true" className="heading">my name is {fname}</h1>
+<img src= {img1} alt = "img not found"></img>
+<a href={links} target="">click me</a>
+</>,
+
+
+
+
+
+document.getElementById("root"));
